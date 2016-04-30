8f6264f7476720a820502632aee5737308ff1d9f
Neha-Jadhav <jneha994@gmail.com>
2016-04-30 17:46:38 +0530

    Update Use.java

--- Rubicon Red/src/com/Use.java
+++ Rubicon Red/src/com/Use.java
@@ -6,23 +6,27 @@ import java.io.IOException;
 public class Use extends Thread {
 	
 	public void run(){
-		Customer c=new Customer(11,"Abhimanyu",1,"Light customer");
+		Customer c=new Customer(1,"Abhimanyu","Light customer");
 		c.display();
+		
 		Shop sc=new Shop();
+		synchronized (this) {
+			
 		try {
 			sc.shopping();
+			sc.cart();
 		} catch (IOException e) {
 			System.out.println("Shopping exception:"+e);
 			e.printStackTrace();
 		}
 	}
+	}
 	
 	public static void main(String[] args) {
 		Use u=new Use();
-		
 		Thread t1=new Thread(u);
-		
 		t1.start();
+		
 		
 		
 	   
